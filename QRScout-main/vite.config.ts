5de07c465daccdc6885766b3f32f5e4f3e32ae20
OrangeCate851 <orangecate47@gmail.com>
2026-03-13 17:37:52 -0400

    Updated PNPM

--- QRScout-main/vite.config.ts
+++ QRScout-main/vite.config.ts
@@ -2,12 +2,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/QRScout/',
   plugins: [
     react(),
+    viteSingleFile(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: false,
@@ -22,4 +24,8 @@ export default defineConfig({
     },
   },
   assetsInclude: ['assets/**/*'],
+  build: {
+    assetsInlineLimit: 100000000,
+    cssCodeSplit: false
+  },
 });
